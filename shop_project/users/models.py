bdef83f9d7864b771b6762057a83cbf3928d2aaa
SkrytnayaRediska <olga@arusnavi.ru>
2023-03-28 21:46:57 +0300

    add catalog models, fix superusers permissions

--- shop_project/users/models.py
+++ shop_project/users/models.py
@@ -18,6 +18,7 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
         return self.create_user(email, password, **extra_fields)
 
 
@@ -29,6 +30,7 @@ class CustomUser(AbstractBaseUser):
     cashback_point = models.IntegerField(default=0)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
@@ -38,3 +40,9 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
